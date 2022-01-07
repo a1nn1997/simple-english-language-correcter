45075be2971e1f96ec82ea29e4007766d79d2d8e
Aniruddh Singh <93520896+a1nn1997@users.noreply.github.com>
2022-01-07 23:17:35 +0530

    Add files via upload

---
+++ grammerchecker.py
@@ -0,0 +1,16 @@
+
+def englishgrammercorrecter(text):
+    """"LanguageTool offers spell and grammar checking.
+       Just paste your text here and your errors will be corrected"""
+    from gingerit.gingerit import GingerIt
+
+    parser = GingerIt()
+    text = parser.parse(text)
+    return str(text["result"])
+
+text=open("grammerchecker.txt","r+")
+t=text.read()
+t1=englishgrammercorrecter(t)
+text.write("\n")
+text.write(t1)
+text.close()
